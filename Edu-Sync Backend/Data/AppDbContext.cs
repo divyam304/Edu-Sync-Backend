20fb7bfc1b4ac979a9523433db7fe368bfc8995a
Divyam Agrawal <91681380+divyam304@users.noreply.github.com>
2025-06-02 12:29:53 +0530

    Update AppDbContext.cs

--- Edu-Sync Backend/Data/AppDbContext.cs
+++ Edu-Sync Backend/Data/AppDbContext.cs
@@ -26,7 +26,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=AGRAWAL-DIVYAM;Initial Catalog=Edu-Sync db;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        => optionsBuilder.UseSqlServer("Server=tcp:edu-sync-server.database.windows.net,1433;Initial Catalog=edu_sync_db;Persist Security Info=False;User ID=azureuser;Password=Password@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
